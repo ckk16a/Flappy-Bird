4644c4859a494533421915e2c5dd998727503f60
Carson Kelley <ckk16a@acu.edu>
2021-02-05 13:31:50 -0600

    Complete Game Maybe

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -29,7 +29,7 @@ public class Bird : MonoBehaviour
                 anim.SetTrigger("Flap");
                 //...zero out the birds current y velocity before...
                 rb2d.velocity = Vector2.zero;
-                //    new Vector2(rb2d.velocity.x, 0);
+                new Vector2(rb2d.velocity.x, 0);
                 //..giving the bird some upward force.
                 rb2d.AddForce(new Vector2(0, upForce));
             }
@@ -39,12 +39,12 @@ public class Bird : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         // Zero out the bird's velocity
-       // rb2d.velocity = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
         // If the bird collides with something set it to dead...
         isDead = true;
         //...tell the Animator about it...
         anim.SetTrigger ("Die");
         //...and tell the game control about it.
-       // GameControl.instance.BirdDied ();
+        GameControl.instance.BirdDied ();
     }
 }
